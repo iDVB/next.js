cf2a535255d1be15ec2c07bf7618e73394718397
Dan Van Brunt <dvanbrunt@gmail.com>
2023-10-27 17:15:21 +0000

    feat(examples): Update i18n-routing w rewrites

--- examples/i18n-routing/next.config.js
+++ examples/i18n-routing/next.config.js
@@ -8,6 +8,34 @@ const nextConfig = {
     locales: ['en', 'fr', 'nl'],
     defaultLocale: 'en',
   },
+  rewrites: async () => {
+    return [
+      {
+        source: '/rewriteWithLocale',
+        destination: 'https://jsonplaceholder.typicode.com/posts/1',
+      },
+      {
+        source: '/rewriteWithoutLocale',
+        destination: 'https://jsonplaceholder.typicode.com/posts/1',
+        locale: false,
+      },
+    ]
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/redirectWithLocale',
+        destination: 'https://cnn.com/business',
+        permanent: false,
+      },
+      {
+        source: '/redirectWithoutLocale',
+        destination: 'https://cnn.com/business',
+        permanent: false,
+        locale: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
